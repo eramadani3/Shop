f5c8eb0a3109eccdd882d3111b3abe1f4b6d3bc4
Engjell Ramadani <eramadani@wpi.edu>
2022-08-01 19:31:54 -0400

    Routed all pages, added authentication, category and size filtering

--- user/src/App.jsx
+++ user/src/App.jsx
@@ -4,9 +4,27 @@ import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
 
 const App = () => {
-  return <Register/>
+  const user = true;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/products/:category" element={<ProductList/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/login" element={user ? <Home/>: <Navigate to="/login" replace/>}/>
+        <Route path="/register" element={user?<Home/>: <Navigate to="/register" replace/>}/>
+      </Routes>
+    </Router>
+  )
 };
 
 export default App;
--- user/src/components/Products.jsx
+++ user/src/components/Products.jsx
@@ -1,16 +1,42 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import styled from "styled-components"
 import { popularProducts } from "../data"
 import Product from "./Product";
+import axios from "axios"
 const Container = styled.div`
     padding: 20px;
     display: flex;
     flex-wrap:wrap;
     justify-content: space-between;
 `;
-const Products = () => {
+const Products = ({cat,filters,sort}) => {
+    const [products,setProducts]=useState([]);
+    const [filteredProducts,setFilteredProducts]=useState([]);
+
+    useEffect(()=>{
+        const getProducts= async () =>{
+            try{
+                const res = await axios.get(cat ?"http://localhost:5000/api/products?category=${cat}" : "http://localhost:5000/api/products" );
+                setProducts(res.data)
+            }catch(err){}
+        };
+        getProducts()
+    },[cat]);
+
+    useEffect(()=>{
+        cat && 
+            setFilteredProducts(
+                products.filter(item=> 
+                    Object.entries(filters).every(([key,value])=>
+                    item[key].includes(value)
+                )
+            )
+        );
+    },[products,cat,filters]);
     return (
         <Container>
-            {popularProducts.map((item) => (
+            {filteredProducts.map((item) => (
                 <Product item={item} key={item.id}/>
             ))}
         </Container>
